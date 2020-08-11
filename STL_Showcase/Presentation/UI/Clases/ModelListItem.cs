e5243278f8434213526c7793c7e3e4a80cb269ff
Alberto Ric Aguarod <dizzzzz@hotmail.com>
2020-08-11 15:03:46 +0200

    Added background colors for the selected image in the image list (its temporal until better effect is made).

--- STL_Showcase/Presentation/UI/Clases/ModelListItem.cs
+++ STL_Showcase/Presentation/UI/Clases/ModelListItem.cs
@@ -1,4 +1,6 @@
 ﻿using STL_Showcase.Logic.Files;
+using STL_Showcase.Shared.Enums;
+using STL_Showcase.Shared.Main;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,8 +29,44 @@ namespace STL_Showcase.Presentation.UI.Clases
         private static int MinImageSize { get; set; } = 32;
 
         bool _IsSelected;
-        public bool IsSelected { get { return _IsSelected; } set { _IsSelected = value; NotifyPropertyChanged(nameof(IsSelected)); } }
+        public bool IsSelected {
+            get { return _IsSelected; }
+            set
+            {
+                _IsSelected = value;
 
+                if (!IsSelected)
+                    _BackgroundColor = Colors.White;
+                else
+                    switch ((RenderAspectEnum)DefaultFactory.GetDefaultUserSettings().GetSettingInt(Shared.Enums.UserSettingEnum.Thumbnails3DAspect))
+                    {
+                        default:
+                        case RenderAspectEnum.PerNormal:
+                            _BackgroundColor = Color.FromRgb(186, 0, 12); break;
+                        case RenderAspectEnum.CyanBlue:
+                            _BackgroundColor = Color.FromRgb(170, 170, 0); break;
+                        case RenderAspectEnum.GreenLimeYellow:
+                            _BackgroundColor = Color.FromRgb(76, 48, 171); break;
+                        case RenderAspectEnum.PinkFucsiaViolet:
+                            _BackgroundColor = Color.FromRgb(39, 179, 115); break;
+                        case RenderAspectEnum.RedOrangeYellow:
+                            _BackgroundColor = Color.FromRgb(27, 176, 92); break;
+                        case RenderAspectEnum.RedRedish:
+                            _BackgroundColor = Color.FromRgb(75, 186, 28); break;
+                        case RenderAspectEnum.VioletBlue:
+                            _BackgroundColor = Color.FromRgb(38, 183, 41); break;
+                        case RenderAspectEnum.White:
+                            _BackgroundColor = Colors.Black; break;
+                        case RenderAspectEnum.Yellow:
+                            _BackgroundColor = Color.FromRgb(153, 65, 126); break;
+                    }
+
+                NotifyPropertyChanged(nameof(IsSelected)); NotifyPropertyChanged(nameof(BackgroundBrush));
+            }
+        }
+
+        private Color _BackgroundColor;
+        public Brush BackgroundBrush { get { return new SolidColorBrush(_BackgroundColor); } }
 
         public void SetImages(BitmapSource[] images)
         {
